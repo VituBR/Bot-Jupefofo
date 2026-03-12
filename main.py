9d10cc49dd87d887965950b09d1a67beded8b96d
VituBR <Vitusemc@gmail.com>
2026-03-12 20:34:12 -0300

    Implement meme and duck commands
    Added meme command to send random memes from images folder and duck command to fetch duck images from an API.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+import os
 import discord
 from discord.ext import commands
 import random
@@ -30,4 +32,54 @@ async def roll(ctx, dice: str):
 async def heh(ctx, count_heh=5):
     await ctx.send("he" * count_heh)
 
-bot.run("SEU TOKEN")
+@bot.command()
+async def meme(ctx):
+    memes = os.listdir("images")
+
+    raridades = []
+    tipos = []
+
+    for meme in memes:
+        if "lendario" in meme:
+            raridades.append(1)
+            tipos.append("lendario")
+        elif "epico" in meme:
+            raridades.append(3)
+            tipos.append("epico")
+        elif "raro" in meme:
+            raridades.append(6)
+            tipos.append("raro")
+        else:
+            raridades.append(10)
+            tipos.append("comum")
+
+    escolha_index = random.choices(range(len(memes)), weights=raridades, k=1)[0]
+    img = memes[escolha_index]
+    tipo = tipos[escolha_index]
+
+    with open(f'images/{img}', 'rb') as f:
+        picture = discord.File(f)
+
+    if tipo == "lendario":
+        await ctx.send(" W MEME LENDÁRIO ENCONTRADO!")
+    elif tipo == "epico":
+        await ctx.send(" MEME ÉPICO SUPER FUNNY!")
+
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Uma vez que chamamos o comando duck, o programa chama a função get_duck_image_url '''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+
+bot.run("Seu Token")
